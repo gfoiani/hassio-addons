79180bab1cf650d4c800cea848e1cd5b706b472c
Giovanni Foiani <giovanni.foiani@gmail.com>
2024-02-25 19:06:47 +0100

    Refactor miner code Add SIGINT handler

--- duino_miner_hassio_addon/main.py
+++ duino_miner_hassio_addon/main.py
@@ -3,21 +3,32 @@ import subprocess
 
 from sys import argv
 
+import signal
+import sys
+
+def signal_handler(sig, frame):
+  print('Exiting main script')
+  sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
 script, threads_count, username, mining_key = argv
 
+MINER_SCRIPT = "miner.py"
+
 def run_script(script_name, thread_index, username, mining_key):
-    subprocess.run(["python3", script_name, thread_index, username, mining_key])
+  subprocess.run(["python3", script_name, thread_index, username, mining_key])
 
 if __name__ == "__main__":
-    thread_list = []
+  thread_list = []
 
-    for idx in range(0, int(threads_count)):
-      thread = threading.Thread(target=run_script, args=("miner.py", str(idx + 1), username, mining_key))
-      thread_list.append(thread)
+  for idx in range(0, int(threads_count)):
+    thread = threading.Thread(target=run_script, args=(MINER_SCRIPT, str(idx + 1), username, mining_key))
+    thread_list.append(thread)
 
-    for thread in thread_list:
-      thread.start()
-    for thread in thread_list:
-      thread.join()
+  for thread in thread_list:
+    thread.start()
+  for thread in thread_list:
+    thread.join()
 
-    print("All scripts have finished executing.")
+  print("All scripts have finished executing.")
--- duino_miner_hassio_addon/miner.py
+++ duino_miner_hassio_addon/miner.py
@@ -1,119 +1,119 @@
 #!/usr/bin/env python3
-# Duino-Coin HASSIO  Miner. Based on  MineCryptoOnWifiRouter by BastelPichi
-
+# Duino-Coin HASSIO Miner. Based on MineCryptoOnWifiRouter by BastelPichi
 
 import hashlib
 import os
-from socket import socket
-import sys  # Only python3 included libraries
+import socket
+import sys
 import time
 import requests
+from enum import Enum
+import signal
+import sys
 
-from sys import argv
+stop_thread = False  # Flag to signal the thread to stop
+script, idx, username, mining_key = sys.argv
 
-script, idx, username, mining_key = argv
+def signal_handler(sig, frame):
+  global stop_thread
+  stop_thread = True
+  print(f"Exiting Thread {idx}")
+  sys.exit(0)
 
+signal.signal(signal.SIGINT, signal_handler)
 
-soc = socket()
+class Feedback(Enum):
+  GOOD = "GOOD"
+  BAD = "BAD"
+
+DEFAULT_NODE_ADDRESS = "server.duinocoin.com"
+DEFAULT_NODE_PORT = 2813
+SOFTWARE_NAME = "HASSIO Miner"
 
 def current_time():
-    t = time.localtime()
-    current_time = time.strftime("%H:%M:%S", t)
-    return current_time
+  return time.strftime("%H:%M:%S", time.localtime())
 
 def fetch_pools():
-    while True:
-        try:
-            response = requests.get(
-                "https://server.duinocoin.com/getPool"
-            ).json()
-            NODE_ADDRESS = response["ip"]
-            NODE_PORT = response["port"]
-
-            return NODE_ADDRESS, NODE_PORT
-        except Exception as e:
-            print (f"{current_time()}: Error retrieving mining node, retrying in 15s")
-            time.sleep(15)
-
-
-while True:
+  while True:
     try:
-        print(f"{current_time()}: Running Thread {idx}")
-        print(f"{current_time()}: Searching for fastest connection to the server")
-        try:
-            NODE_ADDRESS, NODE_PORT = fetch_pools()
-        except Exception as e:
-            NODE_ADDRESS = "server.duinocoin.com"
-            NODE_PORT = 2813
-            print(f"{current_time()}: Using default server port and address")
-        soc.connect((str(NODE_ADDRESS), int(NODE_PORT)))
-        print(f"{current_time()}: Fastest connection found")
-        server_version = soc.recv(100).decode()
-        print (f"{current_time()}: Server Version: {server_version}")
-        # Mining section
-        while True:
-            soc.send(bytes(
-                "JOB,"
-                + str(username)
-                + ",LOW,"
-                + mining_key,
-                encoding="utf8"))
-
-
-            # Receive work
-            job = soc.recv(1024).decode().rstrip("\n")
-            # Split received data to job and difficulty
-            job = job.split(",")
-            difficulty = job[2]
-
-            hashingStartTime = time.time()
-            base_hash = hashlib.sha1(str(job[0]).encode("ascii"))
-            temp_hash = None
-
-            for result in range(100 * int(difficulty) + 1):
-                # Calculate hash with difficulty
-                temp_hash = base_hash.copy()
-                temp_hash.update(str(result).encode("ascii"))
-                ducos1 = temp_hash.hexdigest()
-
-                # If hash is even with expected hash result
-                if job[1] == ducos1:
-                    hashingStopTime = time.time()
-                    timeDifference = hashingStopTime - hashingStartTime
-                    hashrate = result / timeDifference
-
-                    # Send numeric result to the server
-                    soc.send(bytes(
-                        str(result)
-                        + ","
-                        + str(hashrate)
-                        + ",HASSIO Miner",
-                        encoding="utf8"))
-
-                    # Get feedback about the result
-                    feedback = soc.recv(1024).decode().rstrip("\n")
-                    # If result was good
-                    if feedback == "GOOD":
-                        print(f"{current_time()} : Accepted share",
-                              result,
-                              "Hashrate",
-                              int(hashrate/1000),
-                              "kH/s",
-                              "Difficulty",
-                              difficulty)
-                        break
-                    # If result was incorrect
-                    elif feedback == "BAD":
-                        print(f"{current_time()}: Rejected share",
-                              result,
-                              "Hashrate",
-                              int(hashrate/1000),
-                              "kH/s",
-                              "Difficulty",
-                              difficulty)
-                        break
-
+      response = requests.get(f"https://{DEFAULT_NODE_ADDRESS}/getPool").json()
+      NODE_ADDRESS = response["ip"]
+      NODE_PORT = response["port"]
+      return NODE_ADDRESS, NODE_PORT
     except Exception as e:
-        print(f"{current_time()}: Error occured: {e}, restarting in 5s.")
+      print(f"{current_time()}: Error retrieving mining node, retrying in 15s")
+      time.sleep(15)
+
+def mine(username, mining_key, soc):
+    while not stop_thread:
+      soc.send(bytes(f"JOB,{str(username)},LOW,{mining_key}", encoding="utf8"))
+
+      job = soc.recv(1024).decode().rstrip("\n")
+      job = job.split(",")
+      difficulty = job[2]
+
+      hashingStartTime = time.time()
+      base_hash = hashlib.sha1(str(job[0]).encode("ascii"))
+
+      for result in range(100 * int(difficulty) + 1):
+        temp_hash = base_hash.copy()
+        temp_hash.update(str(result).encode("ascii"))
+        ducos1 = temp_hash.hexdigest()
+
+        if job[1] == ducos1:
+          hashingStopTime = time.time()
+          timeDifference = hashingStopTime - hashingStartTime
+          hashrate = result / timeDifference
+
+          soc.send(bytes(f"{str(result)},{str(hashrate)},{SOFTWARE_NAME}", encoding="utf8"))
+
+          feedback = soc.recv(1024).decode().rstrip("\n")
+
+          if feedback == Feedback.GOOD.value:
+            print(f"{current_time()}: Accepted share",
+                  result,
+                  "Hashrate",
+                  int(hashrate/1000),
+                  "kH/s",
+                  "Difficulty",
+                  difficulty)
+            break
+          elif feedback == Feedback.BAD.value:
+            print(f"{current_time()}: Rejected share",
+                  result,
+                  "Hashrate",
+                  int(hashrate/1000),
+                  "kH/s",
+                  "Difficulty",
+                  difficulty)
+            break
+
+def main():
+  soc = socket.socket()
+
+  while True:
+    try:
+      print(f"{current_time()}: Running Thread {idx}")
+      print(f"{current_time()}: Searching for fastest connection to the server")
+
+      try:
+        NODE_ADDRESS, NODE_PORT = fetch_pools()
+      except Exception as e:
+        NODE_ADDRESS = DEFAULT_NODE_ADDRESS
+        NODE_PORT = DEFAULT_NODE_PORT
+        print(f"{current_time()}: Using default server port: {DEFAULT_NODE_PORT} and address: {DEFAULT_NODE_ADDRESS}")
+
+      soc.connect((str(NODE_ADDRESS), int(NODE_PORT)))
+      print(f"{current_time()}: Fastest connection found")
+      server_version = soc.recv(100).decode()
+      print(f"{current_time()}: Server Version: {server_version}")
+
+      mine(username, mining_key, soc)
+    except Exception as e:
+        print(f"{current_time()}: Error occurred: {e}, restarting in 5s.")
         time.sleep(5)
-        os.execl(sys.executable, sys.executable, *sys.argv)
+        soc.close()
+        soc = socket.socket()
+
+if __name__ == "__main__":
+  main()
