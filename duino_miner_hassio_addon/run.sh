c15e48fdd1ffa4dafb06748ea22b0bff9726e74f
Giovanni Foiani <giovanni.foiani@gmail.com>
2024-03-10 15:41:06 +0100

    Update Dockerfile

--- duino_miner_hassio_addon/run.sh
+++ duino_miner_hassio_addon/run.sh
@@ -11,23 +11,6 @@ source ./venv/bin/activate
 echo "Installing requests . . ."
 pip3 install requests
 
-# echo "Downloading Rust . . ."
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs/ | sh -s -- -y
-# source $HOME/.cargo/env
-# echo "Download duino fasthash:"
-# wget https://server.duinocoin.com/fasthash/libducohash.tar.gz
-
-# echo "Unpack it:"
-# tar -xvf libducohash.tar.gz
-# echo "Go to the dir:"
-# cd libducohash
-# echo "Compile it:"
-# rustup target add x86_64-unknown-linux-musl
-# cargo build --target x86_64-unknown-linux-musl --release
-# echo("Extract the module:")
-# mv target/release/libducohasher.so ..
-# cd ..
-
 if [[ $LOCAL_DEPOLY != "true" ]]; then
   USERNAME=$(bashio::config 'username')
   MINING_KEY=$(bashio::config 'mining_key')
